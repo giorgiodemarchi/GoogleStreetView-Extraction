ca31b1cd91f60847bee488c225defed09a893490
giorgiodemarchi <giorgio_demarchi@yahoo.com>
2024-05-06 09:58:00 -0400

    first commit

---
+++ DataCollectionPipeline_v3_full_download.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import os
+
+from utils import (get_street_view_images, 
+                   add_angles, subtract_angles, calculate_orientation,
+                   already_in_dataset, save_data
+)
+
+def generate_images(points_df, point_id, api_key, image_size, directory_name):
+    """ 
+    Return 5 images
+    """
+    p = point_id
+    coordinates = (points_df.iloc[p]['latitude'], points_df.iloc[p]['longitude'])
+    already_present, number_of_points = already_in_dataset(coordinates, directory_name)
+    # Print count once in a while
+    if number_of_points % 100 == 0:
+        print(f"Points in dataset: {number_of_points}")
+
+    if not already_present:
+        ###  Set headings
+        # Get a second point on the same line and compute direction
+        second_point = points_df[(points_df["street_id"] == points_df.iloc[p]["street_id"])
+                                & (points_df["point_id"] != points_df.iloc[p]["point_id"])].iloc[0]
+        second_point_coordinates = (second_point["latitude"], second_point["longitude"])
+        _, angle_baseline = calculate_orientation(coordinates, second_point_coordinates)
+        
+        ## FIRST SIDE
+        ### Do it for one side of the street
+        straight_one = angle_baseline + 90
+        headings_one = [subtract_angles(straight_one, 60), subtract_angles(straight_one, 30), straight_one, add_angles(straight_one, 30), add_angles(straight_one, 60)] 
+        street_view_images_first = get_street_view_images(api_key, coordinates, image_size, headings_one)
+        metadata_one = {'p':p, 
+                        'angle':straight_one,
+                        'latitude': coordinates[0],
+                        'longitude': coordinates[1],
+                        'headings': headings_one,
+                        'address': 'N/A'} # address}
+        
+        # SECOND SIDE
+        straight_two = angle_baseline - 90
+        headings_two = [subtract_angles(straight_two, 60), subtract_angles(straight_two, 30),  straight_two,  add_angles(straight_two, 30), add_angles(straight_two, 60)] 
+        street_view_images_second = get_street_view_images(api_key, coordinates, image_size, headings_two)
+        metadata_two = {'p':p, 
+                'angle':straight_two,
+                'latitude': coordinates[0],
+                'longitude': coordinates[1],
+                'headings': headings_two,
+                'address': 'N/A'} # address}
+
+        return [street_view_images_first, street_view_images_second], [metadata_one, metadata_two]
+    
+    # Else
+    return None, None
+
+def update_tracking_csv(point, len_points):
+    """
+    Keep tracks on what is in dataset and how many images
+    """
+    tracking_df = pd.read_csv('pipe_tracking.csv', index_col=0)
+    tracking_df.loc[tracking_df['point_id'] == point, 'Images'] = len_points
+    tracking_df.loc[tracking_df['point_id'] == point, 'In Dataset'] = 1
+    tracking_df.to_csv('pipe_tracking.csv')
+
+def print_status(i):
+    """
+    Print status of pipeline on terminal
+    """
+    print("-----------------------")
+    print(f"- Iteration of the run: {i}")
+    tracking_df = pd.read_csv('pipe_tracking.csv', index_col=0)
+    print(f"In dataset: {tracking_df['In Dataset'].sum()}")
+    print(f"Broken points in dataset: {len(tracking_df[tracking_df['Images'].isin([0,1,2,3,4,5,6,7,8,9])])}")
+    print(f"Remaining to upload: {tracking_df['Images'].isnull().sum()} ")
+
+if __name__=='__main__':
+    IMAGE_SIZE = "640x480"
+    API_KEY = os.environ.get('GOOGLE_API_KEY')
+    DIRECTORY_NAME = 'GoogleDetroitDatabase/'
+
+    # df = pd.read_csv('../Data/LocationSamplingDataset/FullDetroitPointsDataset_v2.csv', index_col=0)[['point_id', 'street_id', 'longitude', 'latitude']]
+    
+    df = pd.read_csv('pipe_tracking.csv', index_col=0)
+    
+    points_ids = df[(df['In Dataset']!=1) & (df['points_in_street']!=1)].point_id.unique()
+
+    i=0
+    for point in points_ids:
+        i+=1
+        data_points, metadata = generate_images(df, point, API_KEY, IMAGE_SIZE, DIRECTORY_NAME)
+        
+        if data_points is not None and len(data_points[0])==5 and len(data_points[1])==5:
+            # SAVE
+            save_data(data_points[0], metadata[0], DIRECTORY_NAME)
+            save_data(data_points[1], metadata[1], DIRECTORY_NAME) 
+            
+            ## Update pipeline tracking dataframe (store as csv)
+            len_points = len(data_points[0]) + len(data_points[1])
+            update_tracking_csv(point, len_points)
+        else:
+            raise ValueError(f'Less than 5 images returned for point {point}')
+
+        if i%50==0:
+            print_status(i)
+
+        if i == 20000:
+            break
---
+++ utils.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import boto3 
+from io import StringIO, BytesIO
+import requests
+from PIL import Image 
+import io
+import math
+import os 
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+# Check if in dataset
+def get_folder_names(directory_name, bucket_name = 'detroit-project-data-bucket'):
+    """
+    Connect to S3 and read all folder (datapoints) names in the images dataset
+    """
+
+    s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    paginator = s3_client.get_paginator('list_objects_v2')
+    response_iterator = paginator.paginate(
+        Bucket=bucket_name,
+        Prefix=directory_name,
+        Delimiter='/'
+    )
+
+    folder_names = []
+    for response in response_iterator:
+        if response.get('CommonPrefixes') is not None:
+            for prefix in response.get('CommonPrefixes'):
+                # Extract the folder name from the prefix key
+                folder_name = prefix.get('Prefix')
+                # Removing the base directory and the trailing slash to get the folder name
+                folder_name = folder_name[len(directory_name):].strip('/')
+                folder_names.append(folder_name)
+
+    return folder_names
+
+def already_in_dataset(coordinates, directory_name):
+    """
+    Check if coordinate point is already in dataset
+
+    Input: Coordinates (lat, lon) --Tuple
+    Output: Boolean TRUE/FALSE
+    
+    """
+    coords_stored = []
+    items = get_folder_names(directory_name) 
+    if len(items)>0:
+        for item in items:
+            lat = item.split("_")[2]
+            lon = item.split("_")[3]
+            coords_stored.append((lat, lon))
+        if coordinates in coords_stored:
+            return 1, len(items)
+    return 0, len(items)
+
+# Extract images
+def get_street_view_images(api_key, location, size, headings, pitch=0, fov=90):
+    """
+    API Call to get Street View images
+    
+    Cost per 1000 requests: $7
+    """
+    images = []
+
+    base_url = "https://maps.googleapis.com/maps/api/streetview"
+    for heading in headings:
+        params = {
+            "key": api_key,
+            "location": f"{location[0]},{location[1]}",
+            "size": size,
+            "heading": heading,
+            "pitch": pitch,
+            "fov": fov,
+            "source": "outdoor"
+        }
+
+        response = requests.get(base_url, params=params)
+        if response.status_code == 200:
+            image_data = io.BytesIO(response.content)
+            images.append(Image.open(image_data))
+        else:
+            raise ValueError(f'Error with Google API at location {location} - status code: {response.status_code}')
+    return images
+
+# A few functions that are necessary to handle angles
+def normalize_angle(angle):
+    """
+    Normalize the angle to be within [0, 360) degrees.
+    """
+    normalized_angle = angle % 360
+    return normalized_angle
+def add_angles(angle1, angle2):
+    """
+    Add two angles, considering the circular nature of angles.
+    """
+    result = normalize_angle(angle1 + angle2)
+    return result
+def subtract_angles(angle1, angle2):
+    """
+    Subtract one angle from another, considering the circular nature of angles.
+    """
+    result = normalize_angle(angle1 - angle2)
+    return result
+def calculate_orientation(coord1, coord2):
+    """
+    Compute N-E-S-W Orientation of the street based on two points belonging in it.
+    """
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    Δλ = lon2 - lon1
+    Δφ = lat2 - lat1
+    # Calculate the angle in radians
+    θ = math.atan2(Δλ, Δφ)
+    # Convert angle to degrees
+    θ_deg = θ * (180.0 / math.pi)
+    # Normalize the angle to be between 0 and 360 degrees
+    normalized_angle = (θ_deg + 360) % 360
+    # Determine cardinal direction
+    cardinal_directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    index = int((normalized_angle + 22.5) / 45) % 8
+    direction = cardinal_directions[index]
+    # Determine the angle at which the street is directed (using the API standards)
+    angle_google_api = [0, 45, 90, 135, 180, 225, 270, 315]
+    baseline_angle = angle_google_api[index]
+    return direction, baseline_angle
+
+# Save images
+def save_data(images, metadata, directory_name, bucket_name = 'detroit-project-data-bucket'):
+    """
+    Function to store the images
+    """
+    s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    angle = metadata['angle']
+    p = metadata['p']
+    latitude = metadata['latitude']
+    longitude = metadata['longitude']
+    address = metadata['address']
+    headings = metadata['headings']
+
+    # Save the observation of first side (images + metadata)
+    datapoint_id = f"{p}_{angle}_{latitude}_{longitude}"
+    base_s3_path = f"{directory_name}{datapoint_id}"
+
+    for idx, img in enumerate(images):
+        # Convert the image to bytes
+        img_byte_arr = BytesIO()
+        img.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        # Create a full path for the image
+        img_path = f"{base_s3_path}/image_{idx}.png"
+
+        # Upload the image to S3
+        s3_client.put_object(Bucket=bucket_name, Key=img_path, Body=img_byte_arr)
+
+    # Prepare metadata text
+    metadata_content = f"""Latitude: {latitude}
+Longitude: {longitude}
+Headings: {headings}
+Address: {address}"""
+
+    # Save Metadata to a .txt file
+    metadata_path = f"{base_s3_path}/metadata.txt"
+    s3_client.put_object(Bucket=bucket_name, Key=metadata_path, Body=metadata_content)
